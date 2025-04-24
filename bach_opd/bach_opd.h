44fba31b6e5f198daa2073196fd1f3723a262e42
miao <wonjiajie@outlook.com>
2025-04-24 12:42:49 +0000

    add bach-opd

---
+++ bach_opd/bach_opd.h
@@ -0,0 +1,104 @@
+//
+//  bach_opd.h
+//  YCSB-cpp
+//
+//  Copyright (c) 2025 BACH-OPD HIT MDC GROUP
+//  Modifications Copyright 2025 BACH-OPD HIT MDC GROUP
+//
+
+#ifndef YCSB_C_BACH_OPD_H_
+#define YCSB_C_BACH_OPD_H_
+
+#include <string>
+#include <mutex>
+
+#include "core/db.h"
+#include "utils/properties.h"
+
+//#include <BACH/BACH.h>
+
+namespace ycsbc {
+
+// class BachopdDB : public DB{
+//  public:
+//   BachopdDB() {}
+//   ~BachopdDB() {}
+
+//   void Init();
+
+//   void Cleanup();
+
+//   Status Read(const std::string &table, const std::string &key,
+//               const std::vector<std::string> *fields, std::vector<Field> &result) {
+//     return (this->*(method_read_))(table, key, fields, result);
+//   }
+
+//   Status Scan(const std::string &table, const std::string &key, int len,
+//               const std::vector<std::string> *fields, std::vector<std::vector<Field>> &result) {
+//     return (this->*(method_scan_))(table, key, len, fields, result);
+//   }
+
+//   Status Update(const std::string &table, const std::string &key, std::vector<Field> &values) {
+//     return (this->*(method_update_))(table, key, values);
+//   }
+
+//   Status Insert(const std::string &table, const std::string &key, std::vector<Field> &values) {
+//     return (this->*(method_insert_))(table, key, values);
+//   }
+
+//   Status Delete(const std::string &table, const std::string &key) {
+//     return (this->*(method_delete_))(table, key);
+//   }
+
+//  private:
+//   enum RocksFormat {
+//     kSingleRow,
+//   };
+//   RocksFormat format_;
+
+//   void GetOptions(const utils::Properties &props, BACH::Options *opt,
+//                   std::vector<BACH::ColumnFamilyDescriptor> *cf_descs);
+//   static void SerializeRow(const std::vector<Field> &values, std::string &data);
+//   static void DeserializeRowFilter(std::vector<Field> &values, const char *p, const char *lim,
+//                                    const std::vector<std::string> &fields);
+//   static void DeserializeRowFilter(std::vector<Field> &values, const std::string &data,
+//                                    const std::vector<std::string> &fields);
+//   static void DeserializeRow(std::vector<Field> &values, const char *p, const char *lim);
+//   static void DeserializeRow(std::vector<Field> &values, const std::string &data);
+
+//   Status ReadSingle(const std::string &table, const std::string &key,
+//                     const std::vector<std::string> *fields, std::vector<Field> &result);
+//   Status ScanSingle(const std::string &table, const std::string &key, int len,
+//                     const std::vector<std::string> *fields,
+//                     std::vector<std::vector<Field>> &result);
+//   Status UpdateSingle(const std::string &table, const std::string &key,
+//                       std::vector<Field> &values);
+//   Status MergeSingle(const std::string &table, const std::string &key,
+//                      std::vector<Field> &values);
+//   Status InsertSingle(const std::string &table, const std::string &key,
+//                       std::vector<Field> &values);
+//   Status DeleteSingle(const std::string &table, const std::string &key);
+
+//   Status (RocksdbDB::*method_read_)(const std::string &, const std:: string &,
+//                                     const std::vector<std::string> *, std::vector<Field> &);
+//   Status (RocksdbDB::*method_scan_)(const std::string &, const std::string &,
+//                                     int, const std::vector<std::string> *,
+//                                     std::vector<std::vector<Field>> &);
+//   Status (RocksdbDB::*method_update_)(const std::string &, const std::string &,
+//                                       std::vector<Field> &);
+//   Status (RocksdbDB::*method_insert_)(const std::string &, const std::string &,
+//                                       std::vector<Field> &);
+//   Status (RocksdbDB::*method_delete_)(const std::string &, const std::string &);
+
+//   int fieldcount_;
+
+//   static std::vector<BACH::ColumnFamilyHandle *> cf_handles_;
+//   static BACH::DB *db_;
+//   static int ref_cnt_;
+//   static std::mutex mu_;
+// };
+
+// DB *NewRocksdbDB();
+
+} //ycsbc
+#endif //YCSB_C_BACH_OPD_H_
