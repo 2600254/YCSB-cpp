d5bb08e44a5f0cee18199d77595ddfcf6274aad4
2600254 <40998472+2600254@users.noreply.github.com>
2025-05-17 17:07:31 +0000

    rewrite opd's operator

--- bach_opd/bach_opd.h
+++ bach_opd/bach_opd.h
@@ -29,67 +29,38 @@ class BachopdDB : public DB{
   void Cleanup();
 
   Status Read(const std::string &table, const std::string &key,
-              const std::vector<std::string> *fields, std::vector<Field> &result) {
-    return (this->*(method_read_))(table, key, fields, result);
-  }
+              const std::vector<std::string> *fields, std::vector<Field> &result);
 
   Status Scan(const std::string &table, const std::string &key, int len,
-              const std::vector<std::string> *fields, std::vector<std::vector<Field>> &result) {
-    return (this->*(method_scan_))(table, key, len, fields, result);
-  }
+              const std::vector<std::string> *fields, std::vector<std::vector<Field>> &result);
 
-  Status Update(const std::string &table, const std::string &key, std::vector<Field> &values) {
-    return (this->*(method_update_))(table, key, values);
-  }
+  Status Update(const std::string &table, const std::string &key, std::vector<Field> &values);
 
-  Status Insert(const std::string &table, const std::string &key, std::vector<Field> &values) {
-    return (this->*(method_insert_))(table, key, values);
-  }
+  Status Insert(const std::string &table, const std::string &key, std::vector<Field> &values);
 
-  Status Delete(const std::string &table, const std::string &key) {
-    return (this->*(method_delete_))(table, key);
-  }
+  Status Delete(const std::string &table, const std::string &key);
+
+  Status Filter(const std::string &table, const std::vector<DB::Field> &lvalue,
+                const std::vector<DB::Field> &rvalue, const std::vector<std::string> *fields, 
+                std::vector<std::vector<Field>> &result);
 
  private:
 
   void GetOptions(const utils::Properties &props, std::shared_ptr<BACH::Options> opt);
-  static void SerializeRow(const std::vector<Field> &values, BACH::Tuple &data);
+  inline void SerializeRow(const std::vector<Field> &values, BACH::Tuple &data);
 
 
-  static void DeserializeRowFilter(std::vector<Field> &values, const BACH::Tuple &data,
+  inline void DeserializeRowFilter(std::vector<Field> &values, const BACH::Tuple &data,
                                    const std::vector<std::string> &fields);
 
-  static void DeserializeRow(std::vector<Field> &values, const BACH::Tuple &data);
-
-  Status ReadSingle(const std::string &table, const std::string &key,
-                    const std::vector<std::string> *fields, std::vector<Field> &result);
-  Status ScanSingle(const std::string &table, const std::string &key, int len,
-                    const std::vector<std::string> *fields,
-                    std::vector<std::vector<Field>> &result);
-  Status UpdateSingle(const std::string &table, const std::string &key,
-                      std::vector<Field> &values);
-  Status MergeSingle(const std::string &table, const std::string &key,
-                     std::vector<Field> &values);
-  Status InsertSingle(const std::string &table, const std::string &key,
-                      std::vector<Field> &values);
-  Status DeleteSingle(const std::string &table, const std::string &key);
-
-  Status (BachopdDB::*method_read_)(const std::string &, const std:: string &,
-                                    const std::vector<std::string> *, std::vector<Field> &);
-  Status (BachopdDB::*method_scan_)(const std::string &, const std::string &,
-                                    int, const std::vector<std::string> *,
-                                    std::vector<std::vector<Field>> &);
-  Status (BachopdDB::*method_update_)(const std::string &, const std::string &,
-                                      std::vector<Field> &);
-  Status (BachopdDB::*method_insert_)(const std::string &, const std::string &,
-                                      std::vector<Field> &);
-  Status (BachopdDB::*method_delete_)(const std::string &, const std::string &);
+  inline void DeserializeRow(std::vector<Field> &values, const BACH::Tuple &data);
 
   int fieldcount_;
 
   static std::unique_ptr<BACH::DB> db_;
   static int ref_cnt_;
   static std::mutex mu_;
+  std::string field_prefix_;
 };
 
 DB *NewBachopdDB();
